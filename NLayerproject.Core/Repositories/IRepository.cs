a586a6515622ddec01931c0342c2c6892e2582ed
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 17:45:48 +0300

    Generic IRepository

--- NLayerproject.Core/Repositories/IRepository.cs
+++ NLayerproject.Core/Repositories/IRepository.cs
@@ -11,7 +11,7 @@ namespace NLayerproject.Core.Repositories
     {
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
 
         Task AddAsync(TEntity entity);
