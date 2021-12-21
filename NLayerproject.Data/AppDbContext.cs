407b4a325581a1a10fc4d5cd6ec255f3f5b4ca9b
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 17:02:51 +0300

    DbContext

--- NLayerproject.Data/AppDbContext.cs
+++ NLayerproject.Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayerproject.Core.Models;
 using NLayerproject.Data.Configurations;
+using NLayerproject.Data.Seeds;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,10 @@ namespace NLayerproject.Data
         {
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductSeed(new int[] {1,2}));
+            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] {1,2}));
+
         }
     }
 }
