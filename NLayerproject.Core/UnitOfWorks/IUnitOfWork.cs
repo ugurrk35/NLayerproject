31e7262147d3dd8d105ff258f8aa2b2ddd4131f1
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 15:56:04 +0300

    Unit Of Work Interface

---
+++ NLayerproject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using NLayerproject.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Products { get; }   //bunlar burada vermeyebiliriz dependency injection ile beraber kullanabilir
+        ICategoryRepository Categories { get; }
+       
+        Task CommitAsync();  //implement ettiğimiz save change metodu çağıracak
+
+        void Commit();
+    }
+}
