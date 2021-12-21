575c72b83727d55c0225ba4bb503732282bf8dd6
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 17:03:56 +0300

    Category Seed Data

---
+++ NLayerproject.Data/Seeds/CategorySeed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerproject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Data.Seeds
+{
+    public class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        private readonly int[] _Ids;
+        public CategorySeed(int[] Ids)
+        {
+            _Ids= Ids;
+        }
+
+
+
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category
+                {
+                    Id = _Ids[0],
+                    Name ="Kalemler"
+
+                },
+                 new Category
+                 {
+                     Id = _Ids[1],
+                     Name = "Defterler"
+
+                 }
+                );
+        }
+    }
+}
