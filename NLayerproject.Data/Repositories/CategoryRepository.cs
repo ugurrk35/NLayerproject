a36d211c449116a523d196521a401cfbec5c3af4
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 18:55:05 +0300

    Category Repository

---
+++ NLayerproject.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerproject.Core.Models;
+using NLayerproject.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Data.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public CategoryRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetWithProductByIdAsync(int categoryId)
+        {
+            return await _appDbContext.Categories.Include(x => x.Products).SingleOrDefaultAsync(x => x.Id == categoryId);
+        }
+    }
+}
