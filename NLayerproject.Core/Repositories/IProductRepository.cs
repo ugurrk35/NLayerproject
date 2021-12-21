479cf459e34232baa1467d43828c58c5e615b003
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 15:44:43 +0300

    Repository

---
+++ NLayerproject.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using NLayerproject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Core.Repositories
+{
+    public interface IProductRepository:IRepository<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
