0458d496efee18b4ef678c477e0a5072cea51952
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 15:46:41 +0300

    Services

---
+++ NLayerproject.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using NLayerproject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Core.Services
+{
+    public  interface ICategoryService:IService<Category>
+    {
+        Task<Category> GetWithProductByIdAsync(int categoryId);
+        // category ozgu metodlar ,hesaplarlamalar,veritabanına baglantı olmadan helper ozel url vb.
+    }
+}
---
+++ NLayerproject.Core/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using NLayerproject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Core.Services
+{
+    public interface IProductService:IService<Product>
+    {
+
+        //veritabanı ile ilgili olmayan metodlar
+        // bool ControlInnerBarcode(Product product);
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
