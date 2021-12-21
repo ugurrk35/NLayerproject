b8088a42230dab1a2b4eeedc95e128ec041f8653
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 16:39:57 +0300

    Data Configurations

---
+++ NLayerproject.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerproject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+           builder.HasKey(x=>x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");//18 karakterli virgülden sonra 2 basamak alabilsin
+            builder.Property(x=>x.InnerBarcode).HasMaxLength(50);
+            builder.ToTable("Products");
+        }
+    }
+}
