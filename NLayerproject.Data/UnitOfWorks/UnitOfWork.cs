133eb1d73b45f0bb6d78a5ced055b1ea895d745f
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 19:16:17 +0300

    Unit Of Works

---
+++ NLayerproject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using NLayerproject.Core.Repositories;
+using NLayerproject.Core.UnitOfWorks;
+using NLayerproject.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context=appDbContext;
+        }
+        public IProductRepository Products => _productRepository=_productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository=_categoryRepository ?? new CategoryRepository(_context);   
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
