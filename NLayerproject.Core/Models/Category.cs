874dfd8591e6744bbe318f6345044f2d1fe2af71
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-21 13:41:37 +0300

    Add project files.

---
+++ NLayerproject.Core/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Core.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
