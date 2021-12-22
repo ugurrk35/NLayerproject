bf1424f192ac3aed4d0115f06767467ee161f2c5
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-22 13:47:32 +0300

    Gneriric interface repository

--- NLayerproject.Core/Repositories/IRepository.cs
+++ NLayerproject.Core/Repositories/IRepository.cs
@@ -11,7 +11,7 @@ namespace NLayerproject.Core.Repositories
     {
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-        IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task <IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate);
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
 
         Task AddAsync(TEntity entity);
