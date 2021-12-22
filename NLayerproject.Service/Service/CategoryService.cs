832b3de5f6663a1210943945b33a5bef0a6a17ad
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-22 14:30:20 +0300

    Category service

---
+++ NLayerproject.Service/Service/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using NLayerproject.Core.Models;
+using NLayerproject.Core.Repositories;
+using NLayerproject.Core.Services;
+using NLayerproject.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Service.Service
+{
+    public class CategoryService : Service<Category>,ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Category> repository) : base(unitOfWork, repository)
+        {
+
+        }
+        public async Task<Category> GetWithProductByIdAsync(int categoryId)
+        {
+         return await _unitOfWork.Categories.GetWithProductByIdAsync(categoryId);
+        }
+    }
+}
