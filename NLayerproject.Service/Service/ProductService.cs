b178bf762fc71aba1b3077313def5ffb131c99f2
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-22 13:46:26 +0300

    Product service

---
+++ NLayerproject.Service/Service/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using NLayerproject.Core.Models;
+using NLayerproject.Core.Repositories;
+using NLayerproject.Core.Services;
+using NLayerproject.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerproject.Service.Service
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        public ProductService(IUnitOfWork unitOfWork,IRepository<Product> repository):base(unitOfWork,repository)   
+        {
+
+        }
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+           return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
+        }
+    }
+}
