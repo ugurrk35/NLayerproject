8be7144183dfe2191a45ec8913f7660d8d8ebf51
Uğur Keskin <67828030+ugurrk35@users.noreply.github.com>
2021-12-28 20:36:35 +0300

    Filter

---
+++ NLayerproject.API/Filters/NotFoundFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerproject.API.DTOs;
+using NLayerproject.Core.Services;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerproject.API.Filters
+{
+    public class NotFoundFilter:ActionFilterAttribute
+    {
+
+        private readonly IProductService _productService;
+        public NotFoundFilter(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            int id =(int)context.ActionArguments.Values.FirstOrDefault();
+            
+            var product = await _productService.GetByIdAsync(id);
+
+            if(product != null)
+            {
+               await next();
+            }
+            else
+            {
+                ErrorDto errorDto = new ErrorDto();
+                errorDto.Status = 404;
+                errorDto.Errors.Add($"id'si {id} olan ürün bulunamadı");
+                context.Result = new NotFoundObjectResult(errorDto);
+
+            }
+        }
+
+    }
+}
---
+++ NLayerproject.API/Filters/ValidationFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NLayerproject.API.DTOs;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NLayerproject.API.Filters
+{
+    public class ValidationFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                ErrorDto errorDto = new ErrorDto();
+                errorDto.Status = 400;
+                IEnumerable<ModelError> modelErrors = context.ModelState.Values.SelectMany(v => v.Errors);
+
+                modelErrors.ToList().ForEach(x =>
+                {
+                    errorDto.Errors.Add(x.ErrorMessage);
+                });
+                context.Result = new BadRequestObjectResult(errorDto);
+            };
+        }
+    }
+}
